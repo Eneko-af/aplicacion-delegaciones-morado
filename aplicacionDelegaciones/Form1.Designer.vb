31fe05e340c8541363ba9331495dde981ed2060c
Eneko A <arrizabalagae@gmail.com>
2024-01-16 11:46:10 +0100

    solucion a formulario borroso

--- aplicacionDelegaciones/Form1.Designer.vb
+++ aplicacionDelegaciones/Form1.Designer.vb
@@ -30,54 +30,69 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(54, 63)
+        Me.Button1.AutoSize = True
+        Me.Button1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(41, 44)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(150, 60)
+        Me.Button1.Size = New System.Drawing.Size(250, 38)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Gestion de comerciales"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(54, 193)
+        Me.Button2.AutoSize = True
+        Me.Button2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(41, 136)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(150, 60)
+        Me.Button2.Size = New System.Drawing.Size(250, 94)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Gestion de partners"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(349, 63)
+        Me.Button3.AutoSize = True
+        Me.Button3.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(321, 44)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(150, 60)
+        Me.Button3.Size = New System.Drawing.Size(250, 38)
         Me.Button3.TabIndex = 2
         Me.Button3.Text = "Gestion de transportistas"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(349, 193)
+        Me.Button4.AutoSize = True
+        Me.Button4.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(321, 136)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(150, 60)
+        Me.Button4.Size = New System.Drawing.Size(250, 94)
         Me.Button4.TabIndex = 3
         Me.Button4.Text = "Gestion de articulos"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.BackgroundImage = Global.aplicacionDelegaciones.My.Resources.Resources.descarga
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(982, 513)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.Name = "Form1"
-        Me.Text = "Aplicacion de delegaciones"
+        Me.Text = "Aplicacion delegaciones"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
